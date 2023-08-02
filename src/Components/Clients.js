854dd503684973b5f01c55f904bef4c4a2764c46
margielajake <campbell.jake14@gmail.com>
2023-08-03 00:05:47 +0100

    new commit

--- src/Components/Clients.js
+++ src/Components/Clients.js
@@ -2,8 +2,18 @@ import { clients } from "../constants/data";
 
 const Clients = () => {
   return (
-    <div>
-   
+    <div className="clients-section">
+      <p>
+        Powering the world’s best product teams. From next-gen startups to
+        established enterprises.
+      </p>
+      <div>
+        {clients.map((client) => (
+          <div key={client.id} className="flex">
+            <img src={client.logo} alt={client.title} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/constants/data.js
+++ src/constants/data.js
@@ -22,22 +22,27 @@ export const navLinks = [
 export const clients = [
   {
     id: "client-1",
+    title: "loom",
     logo: loom,
   },
   {
     id: "client-2",
+    title: "neara",
     logo: neara,
   },
   {
     id: "client-3",
+    title: "korapay",
     logo: korapay,
   },
   {
     id: "client-4",
+    title: "orchestrate",
     logo: orchestrate,
   },
   {
     id: "client-5",
+    title: "seampay",
     logo: seampay,
   },
 ];
