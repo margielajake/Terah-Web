ecac662c9a35fe2742934c01039b9dd70e7678b6
margielajake <campbell.jake14@gmail.com>
2023-08-04 00:46:37 +0100

    features added

--- src/Components/Clients.js
+++ src/Components/Clients.js
@@ -3,14 +3,14 @@ import { clients } from "../constants/data";
 const Clients = () => {
   return (
     <div className="clients-section">
-      <p>
+      <p className="client-tagline">
         Powering the world’s best product teams. From next-gen startups to
         established enterprises.
       </p>
-      <div>
+      <div className="flex client">
         {clients.map((client) => (
-          <div key={client.id} className="flex">
-            <img src={client.logo} alt={client.title} />
+          <div key={client.id}>
+            <img src={client.logo} alt={client.title} className="client-logo" />
           </div>
         ))}
       </div>
